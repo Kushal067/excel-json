cc56504464c5eacf30bacc77a52af25ad432e341
kushal reddy <kushalreddy67@gmail.com>
2023-02-26 21:08:33 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import * as xlsx from 'xlsx'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: [],
+      isDataAvailable: false,
+      error: null,
+    };
+    
+  }
+
+  componentDidMount=(e)=> {
+    console.log("inside"+this);
+    this.setState({ isDataAvailable: false });
+    //e.preventDefault();
+    if (e) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+          const data = e.target.result;
+          const workbook = xlsx.read(data, { type: "array" });
+          const sheetName = workbook.SheetNames[0];
+          const worksheet = workbook.Sheets[sheetName];
+          xlsx.utils.sheet_to_html(worksheet)
+          const json = xlsx.utils.sheet_to_json(worksheet);
+          console.log(JSON.stringify(json))
+          this.setState({
+            data: json,
+            isDataAvailable : true
+          })
+          this.state.data.forEach(x=>{
+            console.log(JSON.stringify(x))
+          })
+      };
+      reader.readAsArrayBuffer(e.target.files[0]);
+    }
+  }
+
+  render() {
+    const { data, isDataAvailable, error } = this.state;
+    if (error) {
+      return <p>{error.message}</p>;
+    }
+
+    if (!isDataAvailable) {
+      return (
+        <form>
+      <label htmlFor="upload">Upload File</label>
+      <input
+          type="file"
+          name="upload"
+          id="upload"
+          onChange={this.componentDidMount}
+      />
+    </form>
+      );
+    }
+
+    return (
+      
+      <ul>
+         <form>
+          <label htmlFor="upload">Upload File</label>
+          <input
+              type="file"
+              name="upload"
+              id="upload"
+              onChange={this.componentDidMount}
+          />
+        </form>
+        {
+          <table>
+            <tr>
+              <th>Ingestion_ID</th>
+              <th>Master_Dimensions</th>
+              <th>Ingestion_Metadata</th>
+              <th>DQ_Trigger</th>
+              <th>name</th>
+              <th>job_key</th>
+            </tr>
+            {data.map((item =>
+            <tr key={item.Ingestion_ID}>
+              <td>{item.Ingestion_ID}</td>
+              <td>{item.Master_Dimensions}</td>
+              <td>{item.Ingestion_Metadata}</td>
+              <td>{item.DQ_Trigger}</td>
+              <td>{item.name}</td>
+              <td>{item.job_key}</td>
+            </tr>
+            ))}
+          </table>
+        }
+      </ul>
+    );
+  }
 }
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,14 +2,48 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import * as xlsx from 'xlsx'
 import reportWebVitals from './reportWebVitals';
+
+let exceldata=[];
+
+const readUploadFile = (e) => {
+  e.preventDefault();
+  if (e.target.files) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+          const data = e.target.result;
+          const workbook = xlsx.read(data, { type: "array" });
+          const sheetName = workbook.SheetNames[0];
+          const worksheet = workbook.Sheets[sheetName];
+          const json = xlsx.utils.sheet_to_json(worksheet);
+          console.log(json);
+          this.exceldata=json;
+          exceldata.forEach(x=>{
+            console.log(x.Ingestion_ID)
+          })
+      };
+      reader.readAsArrayBuffer(e.target.files[0]);
+  }
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <form>
+    <label htmlFor="upload">Upload File</label>
+    <input
+        type="file"
+        name="upload"
+        id="upload"
+        onChange={readUploadFile}
+    />
+  </form> */}
+  <App/>
   </React.StrictMode>
 );
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
